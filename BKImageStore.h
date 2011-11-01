3e1581312cc1af8c9e405f330fdc36d35e8d03ce
Brian King <brianaking@gmail.com>
2011-10-31 20:47:18 -0400

    Initial release

---
+++ BKImageStore.h
@@ -0,0 +1,168 @@
+/**
+ *   Copyright 2011 King Software Design. All rights reserved.
+ *
+ * @class BKImageStore
+ *
+ * Purpose of BKImageStore is to simplify the saving and loading of camera images in a manner that is memory friendly
+ *  and performant by default.   To that end, these are the goals:
+ *
+ * - Images are identified by an imageID - any unique string
+ *   - Filenames based off of imageID's
+ *   - imageID's with a / are grouped by directory
+ *   - Data Model Independent
+ *
+ * - Minimize main thread processing 
+ *   - Loading, Decompressing and Resizing done off thread
+ *   - Block based API to load images when ready
+ *   - Optionally execute blocks with thumbnails before fetching full sized images
+ *
+ * - Persist images in iOS5 friendly formats
+ *   - Save source images in NSDocuments/<imageID>
+ *   - Save thumbnails of a configurable size in NSDocuments/thumbnails/<imageID>
+ *   - Optionally cache App sized images in NSCache/<imageID> to minimize resize 
+ *      work at expense of disk space
+ *
+ * - Optimal Use of iOS frameworks
+ *   - No UIImage imageWithFileName: - does not persist decompressed UIImage consistently
+ *     - Thanks to @cocoanetics for putting his finger on that puzzle
+ *        http://www.cocoanetics.com/2011/10/avoiding-image-decompression-sickness/
+ *   - Write raw NSData from camera to disk.
+ *   - Always resize images with CGImageSourceCreateThumbnailAtIndex
+ *   - GCD everything, putting every time consuming operation into a separate block to maximize granularity
+ *      - If the UI doesn't care about an object after loading but before re-sizing, it should not resize.
+ *   - Use NSCache to handle low memory warnings
+ *
+ */
+
+#import <Foundation/Foundation.h>
+#import <ImageIO/ImageIO.h>
+
+#define APP_SIZE 480
+
+typedef void (^bk_image_block)(UIImage* img);
+typedef void (^bk_error_block)(NSError* error);
+typedef void (^bk_save_block)();
+
+@interface BKImageStore : NSObject {
+    NSCache *_cache;
+    NSMutableDictionary *_imageBlockEntries;
+    NSMutableSet *_cachedFileSizes;
+    
+    NSUInteger _thumbnailSize;
+    NSUInteger _storageSize;
+    
+    CGFloat _jpegCompressionRatio; 
+    
+    bk_error_block _cacheFileIOErrorHandler;
+}
+/**
+ * Configuration routines
+ * These properties setup the BKImageStore paramaters and should be configured 
+ * directly after instatiation and never changed.
+ */
+
+/**
+ * Size of the thumbnail to generate and to enable thumbnail preloading on cache misses
+ */
+@property (nonatomic, assign) NSUInteger thumbnailSize;
+/**
+ * FIXME: Not yet implemented
+ * Max resolution to save on disk
+ */
+@property (nonatomic, assign) NSUInteger storageSize;
+
+/** 
+ * JPEG compression ratio when saving jpeg's to disk
+ * Defaults to 0.65, my favorite size/quality trade off
+ */
+@property (nonatomic, assign) CGFloat jpegCompressionRatio;
+
+/**
+ * Error block to execute if there are any errors writing 
+ *  files to disk while generating thumbnails or cache sizes
+ */
+@property (nonatomic, copy) bk_error_block cacheFileIOErrorHandler;
+
+/**
+ * Specify a size to cache to disk.  If your app is using one size most of the time
+ *  this will improve loading performance at the expense of save time and disk space.
+ *
+ *  FIXME: Not sure why I chose to have multiple size's here, I doubt it will come in handy
+ *  FIXME: Use CGSize and look at the image orientation to determine proper max size
+ */
+- (void)addCachedFileSize:(NSUInteger)size;
+
+/**
+ * A shared BKImageStore.   Not a singleton as multiple BKImageStore can exist.
+ * FIXME: rename to defaultStore
+ */
++ (BKImageStore *)sharedStore;
+
+/**
+ * Load thumbnail images from disk into cache.
+ *
+ *  Iterate through all of the imageIDs and load the files into cache.  
+ *    If a thumbnail file does not exist, a file will be created from the source file.
+ *
+ *  This call is not asyncronous and CAN be called on or off the main thread.
+ *  This call can only be executed after 
+ */
+- (void)loadThumbnailCacheWithImageIDs:(NSArray *)imageIDs;
+
+/**
+ * Load imageID with the max resolution of maxSize and execute the 
+ *  specified image_block as soon as it is ready.  If the image is not in the cache,
+ *  it will return a token that you can hold onto, and if you don't care about the 
+ *  image_block being executed, run removeImageBlockID:forImageID:.  This will prevent
+ *  the UI from thrashing the image store too much.
+ *  
+ *
+ * In short, this is the algorithm used:
+ *
+ * Look for an image in the cache of _maxSize_ and if it exists, execute image_block
+ * Otherwise:
+ *   If thumbnailSize is set, and a thumbnail image is in the cache, 
+ *       execute image_block(thumbnail) to get some bits on screen
+ *   If the size is in _cachedFileSizes_, and the NSCache/<imageID> file exists
+ *       load the file off main thread, decompress and execute image_block
+ *   If the source file exists on disk, kick off a resize off main thread
+ *       if the size is in _cachedFileSizes_ save it to disk afterwards
+ *         Ideally this never occurs, but can under duress.
+ *   If no image exists, save some state so the image_block is executed
+ *       later when the image is populated with saveImageID:withData:onSave:onError
+ *
+ *  This call can ONLY be called on main thread
+ */
+- (id)loadImageID:(NSString *)imageID 
+            sized:(NSUInteger)maxSize
+            block:(bk_image_block)block;
+
+/**
+ * Save the image represented by _imageData_ to disk as imageID and execute saveBlock || errorBlock
+ *
+ * - Write _imageData_ to disk, 
+ * - Perform any resizes for previous loadImageID:sized:block: calls
+ * - Generate a thumbnail image if thumbnailSize has been set
+ *
+ *  This call can ONLY be called on main thread
+ */
+- (void)saveImageID:(NSString *)imageID 
+           withData:(NSData*)imageData 
+             onSave:(bk_save_block)saveBlock
+            onError:(bk_error_block)errorBlock;
+
+/**
+ * Cancel the operation associated with the loaderID associated with imageID
+ *
+ * FIXME: imageID should not be required
+ */
+- (void)removeImageBlockID:(id)loaderID forImageID:(NSString *)imageID;
+
+/**
+ * Cancel the operation associated with the loaderID.
+ */
+- (void)removeAllImageBlocksForImageID:(NSString *)imageID;
+
+// FIXME: Implement
+//- (void)deleteImageID:(NSString *)imageID;
+@end
---
+++ BKImageStore.m
@@ -0,0 +1,500 @@
+//
+//  ShotLoader.m
+//  lifelapse
+//
+//  Created by Brian King on 2/7/11.
+//  Copyright 2011 King Software Design. All rights reserved.
+//
+
+#import "Shot.h"
+#import "BKImageStore.h"
+
+static dispatch_queue_t __image_store_background_queue;
+
+dispatch_queue_t image_store_background_queue()
+{
+    if (__image_store_background_queue == NULL)
+    {
+        __image_store_background_queue = dispatch_queue_create("com.ksd.image_store.background", 0);
+    }
+    return __image_store_background_queue;
+}
+
+static NSUInteger __loaderCount = 0;
+
+#define CACHE_KEY(imageID, maxSize) [imageID stringByAppendingFormat:@"_%d", maxSize]
+
+@interface BKImageStoreLoader : NSObject {
+    NSUInteger _maxSize;
+    bk_image_block _block;
+    id _loaderID;
+    BOOL _cancel;
+}
+- (id)initWithImageID:(NSString *)imageID size:(NSUInteger)maxSize block:(bk_image_block)block;
+
+@property (readonly) NSString *imageID;
+@property (readonly) id loaderID;
+@property (readonly) NSUInteger maxSize;
+@property (readonly) bk_image_block block;
+@property (assign) BOOL cancel;
+@end
+
+@implementation BKImageStoreLoader
+@synthesize imageID = _imageID;
+@synthesize loaderID = _loaderID;
+@synthesize maxSize = _maxSize;
+@synthesize block = _block;
+@synthesize cancel = cancel;
+- (id)initWithImageID:(NSString *)imageID size:(NSUInteger)maxSize block:(bk_image_block)block;
+{
+    self = [super init];
+    if (self)
+    {
+        _maxSize = maxSize;
+        _block = [block copy];
+        _imageID = [imageID copy];
+        _loaderID = [[NSString stringWithFormat:@"ImageLoader-%d", __loaderCount++] retain];
+        _cancel = NO;
+    }
+    return self;
+}
+- (NSString *)cacheKey
+{
+    return CACHE_KEY(_imageID, _maxSize);
+}
+
+- (void)dealloc
+{
+    [_block release];
+    [_loaderID release];
+    [_imageID release];
+    [super dealloc];
+}
+@end
+
+static BKImageStore *__imageStore = nil;
+
+@implementation BKImageStore
+
+@synthesize thumbnailSize = _thumbnailSize;
+@synthesize storageSize = _storageSize;
+@synthesize cacheFileIOErrorHandler = _cacheFileIOErrorHandler;
+@synthesize jpegCompressionRatio = _jpegCompressionRatio;
+
++ (BKImageStore *)sharedStore
+{
+    if (__imageStore == nil)
+    {
+        __imageStore = [[BKImageStore alloc] init];
+    }
+    return __imageStore;
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self)
+    {
+        _cache = [[NSCache alloc] init];
+        _imageBlockEntries = [[NSMutableDictionary alloc] init];
+        _cachedFileSizes = [[NSMutableSet alloc] init];
+        _thumbnailSize = NSNotFound;
+        _storageSize = NSNotFound;
+        _jpegCompressionRatio = 0.65;
+        _cacheFileIOErrorHandler = [^(NSError *error) {
+            NSLog(@"Error saving a file to cache - %@", error);
+        } copy];
+    }
+    return self;
+}
+- (void)dealloc
+{
+    [_cache release];
+    [_imageBlockEntries release];
+    [_cacheFileIOErrorHandler release];
+    [_cachedFileSizes release];
+    [super dealloc];
+}
+
+- (BOOL)preloadThumbnails
+{
+    return _thumbnailSize != NSNotFound;
+}
+
+- (void)addCachedFileSize:(NSUInteger)size
+{
+    [_cachedFileSizes addObject:[NSNumber numberWithUnsignedInteger:size]];
+}
+#pragma mark -
+#pragma mark Path Accessors
+- (NSString *)thumbnailPathForImageID:(NSString *)imageID
+{
+    NSString *documents = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+    return [[[documents stringByAppendingPathComponent:@"Thumbnails"]
+             stringByAppendingPathComponent:imageID] stringByAppendingPathExtension:@"jpg"];
+
+}
+- (NSString *)cachedPathForKey:(NSString *)key
+{
+    return [[[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:key] stringByAppendingPathExtension:@"jpg"];
+}
+
+- (BOOL)cachedFileExistsForKey:(NSString *)key
+{
+    return [[NSFileManager defaultManager] fileExistsAtPath:[self cachedPathForKey:key]];
+}
+
+- (NSString *)sourcePathForImageID:(NSString *)imageID
+{
+    return [[[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:imageID] stringByAppendingPathExtension:@"jpg"];
+}
+
+- (BOOL)sourceFileExistsForImageID:(NSString *)imageID
+{
+    return [[NSFileManager defaultManager] fileExistsAtPath:[self sourcePathForImageID:imageID]];
+}
+
+#pragma mark -
+#pragma mark Image Cache
+- (UIImage *)cacheImageForKey:(NSString *)key
+{
+    return [_cache objectForKey:key];
+}
+- (void)cacheImage:(UIImage *)image forKey:(NSString *)key
+{
+    [_cache setObject:image forKey:key];
+}
+
+#pragma mark Image Entry Creation
+- (BKImageStoreLoader *)loaderForBlock:(bk_image_block)block imageID:(NSString *)imageID sized:(NSUInteger)maxSize 
+{
+    NSParameterAssert(block);
+    NSParameterAssert(imageID);
+
+    BKImageStoreLoader *loader = [[BKImageStoreLoader alloc] initWithImageID:imageID size:maxSize block:block];
+
+    NSMutableDictionary *entries = [_imageBlockEntries objectForKey:imageID];
+    if (entries == nil)
+    {
+        entries = [NSMutableDictionary dictionary];
+        [_imageBlockEntries setObject:entries forKey:imageID];
+    }
+    [entries setObject:loader forKey:loader.loaderID];
+
+    [loader release];
+    return loader;
+}
+
+- (void)removeAllImageBlocksForImageID:(NSString *)imageID
+{
+    NSParameterAssert(imageID);
+    for (BKImageStoreLoader *loader in [_imageBlockEntries objectForKey:imageID])
+        loader.cancel = YES;
+    
+    [_imageBlockEntries removeObjectForKey:imageID];
+}
+- (void)removeImageBlockID:(id)loaderID forImageID:(NSString *)imageID;
+{
+    NSParameterAssert(imageID);
+    // loaderID will be nil for cache hits
+    if (loaderID == nil)
+        return;
+    
+    NSMutableDictionary *entries = [_imageBlockEntries objectForKey:imageID];
+    BKImageStoreLoader *loader = [entries objectForKey:loaderID];
+    loader.cancel = YES;
+    [entries removeObjectForKey:loaderID];
+    
+    if ([entries count] == 0)
+        [_imageBlockEntries removeObjectForKey:imageID];
+
+}
+- (void)executeThumbnailForLoader:(BKImageStoreLoader *)loader
+{
+    UIImage *thumbnail = [self cacheImageForKey:CACHE_KEY(loader.imageID, self.thumbnailSize)];
+
+    if (thumbnail)
+        loader.block(thumbnail);
+    else
+        NSLog(@"No Thumbnail for imageID: %@", loader.imageID);
+}
+- (void)executeLoader:(BKImageStoreLoader *)loader withImage:(UIImage *)cachedImage;
+{
+    NSParameterAssert(loader);
+    NSParameterAssert(cachedImage);
+    //
+    // Decompress the image off thread
+    //
+    UIGraphicsBeginImageContext(CGSizeMake(1, 1));
+    [cachedImage drawAtPoint:CGPointZero];
+    UIGraphicsEndImageContext();
+
+    //
+    // Execute the loader on the main thread and remove the loader
+    //
+    dispatch_async(dispatch_get_main_queue(), ^{
+        //
+        // If the loader has been cancelled, bail
+        //
+        if (loader.cancel)
+            return;
+
+        loader.block(cachedImage);
+        
+        NSMutableDictionary *entries = [_imageBlockEntries objectForKey:loader.imageID];
+        [entries removeObjectForKey:[loader loaderID]];
+    });
+}
+- (BOOL)saveImageData:(NSData *)imageData toPath:(NSString *)path error:(NSError **)error
+{
+    NSString *directory = [path stringByDeletingLastPathComponent];
+    
+    BOOL ok = [[NSFileManager defaultManager] createDirectoryAtPath:directory
+                                        withIntermediateDirectories:YES
+                                                         attributes:nil 
+                                                              error:error];
+    
+    if (ok)
+        ok = [imageData writeToFile:path options:NSDataWritingAtomic error:error];
+    
+    return ok;
+}
+
+- (void)saveToFile:(NSString *)path image:(UIImage *)image
+{
+    NSParameterAssert(path);
+    NSParameterAssert(image);
+    
+    dispatch_async(image_store_background_queue(), ^{
+        NSData *imageData = UIImageJPEGRepresentation(image, _jpegCompressionRatio);
+        NSError *error = nil;
+        
+        BOOL ok = [self saveImageData:imageData toPath:path error:&error];
+        if (!ok)
+        {
+            self.cacheFileIOErrorHandler(error);
+        }
+    });
+}
+- (UIImage *)imageFromCacheFile:(NSString *)path
+{
+    CFURLRef pathURL = (CFURLRef)[NSURL fileURLWithPath:path];
+    CGImageSourceRef imageSource = CGImageSourceCreateWithURL(pathURL, NULL);
+    
+    CGImageRef imageRef = CGImageSourceCreateImageAtIndex(imageSource, 0, NULL);
+    UIImage *image = [[UIImage alloc] initWithCGImage:imageRef];
+
+    CGImageRelease(imageRef);
+    return [image autorelease];
+}
+- (void)loadFromCacheFile:(BKImageStoreLoader *)loader
+{
+    NSParameterAssert(loader);
+    
+    dispatch_async(image_store_background_queue(), ^{
+        //
+        // Bail if we no longer have an image_block for this ID
+        //
+        if (loader.cancel) return;
+        
+        //
+        // Load the resized image from disk
+        //
+        UIImage *image = [self imageFromCacheFile:[self cachedPathForKey:[loader cacheKey]]];
+        //
+        // Cache the image and execute any pending blocks
+        //
+        [self cacheImage:image forKey:[loader cacheKey]];
+        [self executeLoader:loader withImage:image];
+    });
+}
+- (CFDictionaryRef)resizeOptionsForMaxSize:(NSUInteger)maxSize
+{
+    NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:
+                             (id)kCFBooleanTrue, (id)kCGImageSourceCreateThumbnailWithTransform,
+                             (id)kCFBooleanTrue, (id)kCGImageSourceCreateThumbnailFromImageIfAbsent,
+                             (id)[NSNumber numberWithInt:maxSize], (id)kCGImageSourceThumbnailMaxPixelSize, 
+                             nil];
+    return (CFDictionaryRef)options;
+}
+
+- (void)generateThumbnailFromImageSource:(CGImageSourceRef)imageSource forImageID:(NSString *)imageID
+{
+    NSParameterAssert(imageSource);
+    NSParameterAssert(imageID);
+    NSParameterAssert([self preloadThumbnails]);
+    
+    CGImageRef thumbnailImageRef = 
+        CGImageSourceCreateThumbnailAtIndex(imageSource,
+                                            0, 
+                                            [self resizeOptionsForMaxSize:self.thumbnailSize]);
+    UIImage *thumbnailImage = [[UIImage alloc] initWithCGImage:thumbnailImageRef];
+    
+    [self cacheImage:thumbnailImage forKey:CACHE_KEY(imageID, self.thumbnailSize)];
+    [self saveToFile:[self thumbnailPathForImageID:imageID] image:thumbnailImage];
+
+    CGImageRelease(thumbnailImageRef);
+    [thumbnailImage release];
+}
+
+- (void)loadThumbnailCacheWithImageIDs:(NSArray *)imageIDs
+{
+    for (NSString *imageID in imageIDs)
+    {
+        NSString *path = [self thumbnailPathForImageID:imageID];
+        UIImage *thumbnailImage = [self imageFromCacheFile:path];
+        if (thumbnailImage)
+            [self cacheImage:thumbnailImage forKey:CACHE_KEY(imageID, self.thumbnailSize)];
+        else
+        {
+            //
+            // Something failed when generating a thumbnail, so lets re-generate.
+            //
+            CFURLRef pathURL = (CFURLRef)[NSURL fileURLWithPath:[self sourcePathForImageID:imageID]];
+            CGImageSourceRef imageSource = CGImageSourceCreateWithURL(pathURL, NULL);
+            
+            [self generateThumbnailFromImageSource:nil forImageID:imageID];
+
+            CFRelease(imageSource);
+        }
+
+    }
+}
+
+- (void)resizeImageSourceRef:(CGImageSourceRef)imageSource loader:(BKImageStoreLoader *)loader
+{
+    NSParameterAssert(imageSource);
+    NSParameterAssert(loader);
+
+    CFRetain(imageSource);
+    dispatch_async(image_store_background_queue(), ^{
+        //
+        // Bail if we no longer have an image_block for this ID
+        //
+        if (loader.cancel)
+        {
+            CFRelease(imageSource);
+            return;
+        }
+        
+        //
+        // Resize the image
+        //   
+        CGImageRef imageRef = CGImageSourceCreateThumbnailAtIndex(imageSource,
+                                                                  0, 
+                                                                  [self resizeOptionsForMaxSize:loader.maxSize]);
+        UIImage *image = [[UIImage alloc] initWithCGImage:imageRef];
+        
+      
+        //
+        // Cache the image and execute any pending blocks
+        //
+        [self cacheImage:image forKey:[loader cacheKey]];
+        [self executeLoader:loader withImage:image];
+        
+        //
+        // Save the image if flagged as a 'cached size', and then a thumbnail
+        //
+        if ([_cachedFileSizes containsObject:[NSNumber numberWithUnsignedInteger:loader.maxSize]])
+            [self saveToFile:[self cachedPathForKey:[loader cacheKey]] image:image];
+
+        CGImageRelease(imageRef);
+        [image release];
+
+        CFRelease(imageSource);
+    });
+}
+
+- (id)addLoaderForImageID:(NSString *)imageID sized:(NSUInteger)maxSize block:(bk_image_block)block
+{
+    NSParameterAssert(imageID);
+    NSParameterAssert(block);
+    NSString *key = CACHE_KEY(imageID, maxSize);
+    UIImage *cachedImage = [self cacheImageForKey:key];
+    if (cachedImage)
+    {
+        block(cachedImage);
+        return nil;
+    }
+    else
+    {
+        BKImageStoreLoader *loader = [self loaderForBlock:block imageID:imageID sized:maxSize];
+
+        if ([self preloadThumbnails])
+            [self executeThumbnailForLoader:loader];
+
+        //
+        // Load the cached resize if it exists, otherwise kick off the resize
+        //
+        if ([self cachedFileExistsForKey:key])
+        {
+            [self loadFromCacheFile:loader];
+        }
+        else if ([self sourceFileExistsForImageID:imageID])
+        {
+            CFURLRef pathURL = (CFURLRef)[NSURL fileURLWithPath:[self sourcePathForImageID:imageID]];
+            CGImageSourceRef imageSource = CGImageSourceCreateWithURL(pathURL, NULL);
+
+            [self resizeImageSourceRef:imageSource loader:loader];
+     
+            CFRelease(imageSource);
+        }
+        return loader.loaderID;
+    }
+}
+
+- (void)saveImageID:(NSString *)imageID 
+           withData:(NSData*)imageData 
+             onSave:(bk_save_block)saveBlock
+            onError:(bk_error_block)errorBlock
+{
+    NSParameterAssert(imageID);
+    NSParameterAssert(imageData);
+ 	dispatch_async(image_store_background_queue(), ^{        
+        //
+        // Save Image to disk, creating any needed directories before hand
+        //
+        NSString *path = [self sourcePathForImageID:imageID];
+        NSError *error = nil;
+        BOOL ok = [self saveImageData:imageData toPath:path error:&error];
+        if (!ok)
+        {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                errorBlock(error);
+                [self removeAllImageBlocksForImageID:imageID];
+            });
+            return;
+        }
+        // FIXME: How do I determine the size of imageData without loading too much data so I can resize !
+
+        dispatch_async(dispatch_get_main_queue(), ^{
+            saveBlock();
+        });
+            
+        CGImageSourceRef imageSource = CGImageSourceCreateWithData((CFDataRef)imageData, NULL);
+
+        if ([self preloadThumbnails])
+        {
+            [self generateThumbnailFromImageSource:imageSource forImageID:imageID];
+        }
+
+        //
+        // Kick off resizes in the main thread for any pending BKImageStoreLoader objects
+        //
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSArray *loaders = nil;
+
+            NSMutableDictionary *entries = [_imageBlockEntries objectForKey:imageID];
+            loaders = [entries allValues];
+
+            // Kick off resizes for any block loaders in the system
+            for (BKImageStoreLoader *loader in loaders)
+            {
+                [self resizeImageSourceRef:imageSource loader:loader];
+            }
+            CFRelease(imageSource);
+        });
+    });   
+}
+
+@end
